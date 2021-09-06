1b1fec2b376eb469d86310770f7e6a26fa8c1a69
Youngho <cyh6099@gmail.com>
2021-09-06 17:15:21 +0900

    modify mysql connection -> pool, async/await

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,16 @@
 const dotenv = require('dotenv');
 dotenv.config()
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-module.exports = {
-    connection: mysql.createConnection({
-        host     : 'localhost',
-        database : process.env.DB_NAME,
-        user     : process.env.DB_USER,
-        password : process.env.DB_PASSWORD ,
-        port: process.env.DB_PORT
-    })
+const pool = {
+    host     : 'localhost',
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD ,
+    port: process.env.DB_PORT,
+    database : process.env.DB_NAME,
+    connectionLimit : 30
 }
+
+
+
+module.exports = mysql.createPool(pool);
--- src/router/board.js
+++ src/router/board.js
@@ -1,88 +1,99 @@
 const router = require('express').Router();
-const mysql = require('mysql2');
 const db = require('../config/db')
 
 // 전체 게시글 목록 조회
-router.get('/list',((req, res) => {
+router.get('/list', (async (req, res) => {
+    try {
+        const connection = await db.getConnection(async conn => conn);
 
+        const board_data = await connection.query('SELECT * FROM BOARD');
+        connection.release();
+        res.json(board_data)
 
-    db.connection.query('SELECT * FROM BOARD', function(err, rows, fields) {
-        if (err) throw err;
-        res.json(rows)
-    });
-
+    } catch (err) {
+        res.status(500).send("message : Internal Server Error");
+    }
 }))
 
 // 게시글 건별 조회 - board_id로
-router.get('/:board_id',((req, res) => {
+router.get('/:board_id', (async (req, res) => {
+    try {
+        const connection = await db.getConnection(async conn => conn);
+        let query = 'SELECT * FROM BOARD WHERE board_id = ?'
 
+        const data = await connection.query(query, req.params.board_id);
+        connection.release();
+        res.json(data)
 
-
-    let query = 'SELECT * FROM BOARD WHERE board_id = ?'
-
-    db.connection.query(query, req.params.board_id, function(err, rows, fields) {
-        if (err) throw err;
-        res.json(rows)
-        console.log(rows)
-    });
+    } catch (e) {
+        res.status(500).send("message : Internal Server Error");
+    }
 
 }))
 
 // 게시글 작성 - url query string으로 title, body받고 user_id는 나중에 회원기능 구현하면 수정할 예정
-router.post('/write',((req, res) => {
-    console.log(req.query)
+router.post('/write',(async (req, res) => {
     const{
         title,
         body,
         fk_user_id = 1,
-    } = req.query
-    console.log(title)
-    console.log(body)
-    console.log(fk_user_id)
+    } = req.body
 
+    if(!title || !body){res.status(400).send("message : input something")}
+    else{
+        try{
+            let query = 'INSERT INTO BOARD(title, body, fk_user_id) VALUES(?,?,?)'
+            const connection = await db.getConnection(async conn => conn);
 
+            await connection.query(query,[title,body,fk_user_id]);
+            connection.release()
+            res.status(201).send("message: success create");
+        }
+        catch (e){
+            res.status(500).send("message : Internal Server Error");
+        }
 
-    let query = 'INSERT INTO BOARD(title, body, fk_user_id) VALUES(?,?,?)'
-
-    db.connection.query(query, [title, body, fk_user_id], function(err, rows, fields) {
-        if (err) throw err;
-        res.status(201)
-        console.log(rows)
-    });
-
-
+    }
 }))
 
-router.delete('/delete/:board_id',((req, res) => {
+router.delete('/delete/:board_id',(async (req, res) => {
+    try{
 
-
-    let query = 'DELETE FROM BOARD WHERE board_id = ?'
-
-    db.connection.query(query, req.params.board_id, function(err, rows, fields) {
-        if (err) throw err;
-        res.json(rows)
-        console.log(rows)
-    });
-;
+        const connection = await db.getConnection(async conn => conn);
+        let query = 'DELETE FROM BOARD WHERE board_id = ?'
+        await connection.query(query,req.params.board_id)
+            .then(()=>{
+                res.status(202).send("message: success delete");
+            })
+            .catch((err) => {
+                res.status(500).send("message : Internal Server Error");
+            })
+    }
+    catch (e) {
+        res.status(500).send("message : Internal Server Error");
+    }
 }))
 
 // 게시글 update - 나중에 사용자 검증 기능 필요
-router.put('/update',((req, res) => {
-    const{
-        title,
-        body,
-        board_id,
-    } = req.query
+router.put('/update',(async (req, res) => {
+    try{
+        const{
+            title,
+            body,
+            board_id,
+        } = req.body;
+        const connection = await db.getConnection(async conn => conn);
 
+        const query = 'UPDATE BOARD SET title=?,body=? WHERE board_id = ?'
 
-    let query = 'UPDATE BOARD SET title=?,body=? WHERE board_id = ?'
+        await connection.query(query, [title, body, board_id])
+            .then(() => res.status(201).send("message: success update"))
+            .catch((e)=>res.status(400).send("message : fail"))
 
-    db.connection.query(query, [title,body,board_id], function(err, rows, fields) {
-        if (err) throw err;
-        res.json(rows)
-        console.log(rows)
-    });
-
+    }
+    catch (e) {
+        res.status(500).send("message : Internal Server Error");
+    }
 }))
 
 module.exports = router;
